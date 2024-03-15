0f302b3b192c494eaa507f7ec6d4408211da1af0
HannahhRBX <hannahtucker203@gmail.com>
2024-03-15 10:26:32 +0000

    Initialised App

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+
+    <View style = {styles.container}>
+      <Text>Where is Jack Hine I love him so much he is the best human being alive and I cant live without his amazing humor</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
